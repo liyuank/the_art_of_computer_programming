e737e238f561bd48acfba264023aebcea8d28097
liyuank <12709879+liyuank@users.noreply.github.com>
2017-12-10 23:40:45 -0800

    Add files via upload

---
+++ stars.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+for i in range(1,6):
+    print('*' * i)
+    
+
+
+# In[5]:
+
+
+for i in range(5, 0, -1):
+    print('*' * i)
+
+
+# In[9]:
+
+
+for i in range(1,6):
+    print('{0:>5}'.format('*' * i))
+
+
+# In[10]:
+
+
+for i in range(1,6):
+    print('{0:>10}'.format('*' * i))
+
+
+# In[12]:
+
+
+for i in range(1,11):
+    print('{0:>10}'.format('*' * i))
+
