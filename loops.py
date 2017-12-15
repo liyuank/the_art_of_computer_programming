4722d075045075657695bcb7d6cecdc1e7e40895
liyuank <12709879+liyuank@users.noreply.github.com>
2017-12-15 00:13:25 -0800

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,15 @@
+print("Demo: how to print 9 row using for loop!")
+#start
+for i in range(1, 10):
+    print ("This is {} row.".format(i))
+#end
+print("End of the first program!")
+
+print("Demo: how to use 2 for loops to print!")
+#start
+
+for i in range(1, 5):
+  for j in range(1, 5):
+    print("{} * {} = {}".format(i, j, i*j))
+#end
+print("End of the second program!")
